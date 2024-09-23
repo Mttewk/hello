c612a17c153a0e105668a9a7600b75840a175a38
Mttewk <mttewk@gmail.com>
2024-09-23 17:37:32 +0300

    --

--- hello/main.cpp
+++ hello/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  hello
-//
-//  Created by Матвей Абрамов on 23.09.2024.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
